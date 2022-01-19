4b7676208bdbd5c155306149d6124df68a1d8fac
RetaxMaster <retaxmaster@hotmail.com>
2022-01-19 14:45:53 -0600

    Clase 12

---
+++ for.php
@@ -0,0 +1,12 @@
+<?php
+
+for ($i = 0, $j = 0; $i < 10; $i++, $j += 2) {
+    // Escribir todo nuestro código
+    echo "i = $i j = $j" . "\n";
+}
+
+for ($i=0; $i < 10; $i++) { 
+    # code...
+}
+
+echo "\n";
