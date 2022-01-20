e153b2125f059aee10f977574b4b5763c9918b46
RetaxMaster <retaxmaster@hotmail.com>
2022-01-20 14:47:01 -0600

    Juego finalizado

--- ahorcado.php
+++ ahorcado.php
@@ -1,5 +1,16 @@
 <?php
 
+function clear() {
+
+    if (PHP_OS === "WINNT") {
+        system("cls");
+    }
+    else {
+        system("clear");
+    }
+    
+}
+
 $possible_words = ["Bebida", "Prisma", "Ala", "Dolor", "Piloto", "Baldosa", "Terremoto", "Asteroide", "Gallo", "Platzi"];
 
 define("MAX_ATTEMPS", 6);
@@ -13,14 +24,50 @@ $word_length = strlen($choosen_word);
 $discovered_letters = str_pad("", $word_length, "_");
 $attempts = 0;
 
+do {
 
-echo "Palabra de $word_length letras \n\n";
-echo $discovered_letters . "\n\n";
+    echo "Palabra de $word_length letras \n\n";
+    echo $discovered_letters . "\n\n";
 
-// Pedimos al usuario que escriba
-$player_letter = readline("Escribe una letra: ");
-$player_letter = strtolower($player_letter);
+    // Pedimos al usuario que escriba
+    $player_letter = readline("Escribe una letra: ");
+    $player_letter = strtolower($player_letter);
 
+    if ( str_contains($choosen_word, $player_letter) ) {
 
+        // Verificamos todas las ocurrencias de esta letra para reemplazarla
+        $offset = 0;
+        while(
+            ($letter_position = strpos($choosen_word, $player_letter, $offset)) !== false
+        ) {
+
+            $discovered_letters[$letter_position] = $player_letter;
+            $offset = $letter_position + 1;
+
+        }
+        
+    } 
+    else {
+
+        clear();
+        $attempts++;
+        echo "Letra incorrecta 😾. Te quedan " . (MAX_ATTEMPS - $attempts) . " intentos.";
+        sleep(2);
+
+    }
+
+    clear();
+
+} while( $attempts < MAX_ATTEMPS && $discovered_letters != $choosen_word );
+
+clear();
+
+if ($attempts < MAX_ATTEMPS)
+    echo "¡Felicidades! Has adivinado la palabra. 🙀 \n\n";
+else
+    echo "Suerte para la próxima, amigo. 😾 \n\n";
+
+echo "La palabra es: $choosen_word\n";
+echo "Tú descubriste $discovered_letters";
 
 echo "\n";
